b165f902a63db39f304eec39b3253097f7cd93ff
Jeff Norton <jnorton@bcbsm.com>
2017-09-01 10:53:20 -0400

    Changes for JEE container

--- src/main/java/com/bcbsm/docusignconnectpoc/DocusignConnectPocApplication.java
+++ src/main/java/com/bcbsm/docusignconnectpoc/DocusignConnectPocApplication.java
@@ -1,11 +1,24 @@
 package com.bcbsm.docusignconnectpoc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-public class DocusignConnectPocApplication {
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class DocusignConnectPocApplication extends SpringBootServletInitializer {
+    private static Class<DocusignConnectPocApplication> applicationClass = DocusignConnectPocApplication.class;
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(DocusignConnectPocApplication.class, args);
 	}
